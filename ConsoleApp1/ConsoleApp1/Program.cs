534880ed6b71f1b3d5a466f1474770e67d9ed494
ferasaadatul <ferasc@students.amikom.ac.id>
2023-04-05 16:13:50 +0700

    LatihanClass&Object

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    public class program
+    {
+
+        static void Main(string[] ags)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassanger = "10";
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DropOffPassanger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Taxi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public Boolean OnDuty { get; set; }
+
+        public string NumPassanger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            if (OnDuty)
+            {
+                Console.WriteLine("On Duty: Yes");
+            }
+            else
+            {
+                Console.WriteLine("On Duty: No");
+            }
+            Console.WriteLine("Num Passanger : {0}", NumPassanger);
+
+        }
+
+        public void PickUpPassanger()
+        {
+            Console.WriteLine("{0} Sedang menjemput penumpang\n", DriverName);
+        }
+
+        public void DropOffPassanger()
+        {
+            Console.WriteLine("{0} Selesai mengantar penumpang\n", DriverName);
+        }
+    }
+}
